263b7b5d3ca2b3a51a2f466570996f158f3cc324
ollisomero <olli.somero@codemate.com>
2020-09-24 11:25:44 +0300

    initial fixes

--- draganddropbox/index.js
+++ draganddropbox/index.js
@@ -1,47 +1,78 @@
 window.onload = () => {
+    const DRAG_AND_DROP_BOX_CLASS = 'dragAndDropBox';
+    let dragAndDropBoxes = document.getElementsByClassName(DRAG_AND_DROP_BOX_CLASS);
+
     function preventDefaultActions(event) {
         event.preventDefault();
         event.stopPropagation();
     }
 
-    function highlight(e, dropArea) {
-        dropArea.classList.add('highlight')
+    function highlight(e, dropArea, inputArea) {
+        dropArea.classList.add('highlight');
+        inputArea.className = 'dragAndDrop-input-field-active';
     }
 
-    function unhighlight(e, dropArea) {
-        dropArea.classList.remove('highlight')
+    function unHighlight(e, dropArea, inputArea) {
+        dropArea.classList.remove('highlight');
+        inputArea.className = 'dragAndDrop-input-field-neutral';
     }
 
-    function handleDrop(event) {
+    function handleDrop(event, fileList) {
+        let dragFiles = event?.dataTransfer?.files;
+        dragFiles = Boolean(dragFiles && dragFiles[0]);
+        let buttonFiles = event?.srcElement.files;
+        buttonFiles = Boolean(buttonFiles && buttonFiles[0]);
         let inputNode = event?.srcElement?.children;
-        let uploadFiles = event.dataTransfer.files;
-        inputNode = inputNode[0];
-        inputNode.files = uploadFiles;
+        console.log(event.srcElement);
+        if (dragFiles && inputNode && inputNode[0]) {
+            let uploadFiles = event?.dataTransfer?.files;
+            const inputFile = uploadFiles[0];
+            if (inputFile) {
+                fileList.innerText = `${inputFile.name} (${inputFile.size}kb)`;
+            }
+            inputNode = inputNode[0];
+            if (uploadFiles && uploadFiles[0]) {
+                inputNode.files = uploadFiles;
+            }
+        } else if (buttonFiles) {
+            let inputFile = event.srcElement.files[0];
+            fileList.innerText = `${inputFile.name} (${inputFile.size}kb)`;
+        }
     }
 
-    const DRAG_AND_DROP_BOX_CLASS = 'dragAndDropBox';
-    let dragAndDropBoxes = document.getElementsByClassName(DRAG_AND_DROP_BOX_CLASS);
     Array.from(dragAndDropBoxes).forEach((dropBox, index) => {
         const actions = ['dragenter', 'dragover', 'dragleave', 'drop'];
         const dragActions = ['dragenter', 'dragover'];
         const dragAwayActions = ['dragleave', 'drop'];
+
         let inputWrapper = document.createElement('div');
+        let fileList = document.createElement('div');
         let inputField = document.createElement('input');
+
         actions.forEach(key => {
             inputWrapper.addEventListener(key, preventDefaultActions, false);
         });
+
         dragActions.forEach(key => {
-            inputWrapper.addEventListener(key, e => highlight(e, inputWrapper), false)
+            inputWrapper.addEventListener(key, e => highlight(e, inputWrapper, inputField), false)
         });
+
         dragAwayActions.forEach(key => {
-            inputWrapper.addEventListener(key, e => unhighlight(e, inputWrapper), false);
+            inputWrapper.addEventListener(key, e => unHighlight(e, inputWrapper, inputField), false);
         });
-        inputWrapper.addEventListener('drop', handleDrop, false)
+
+        inputField.addEventListener('change', e => handleDrop(e, fileList), false);
+
+        inputWrapper.addEventListener('drop', e => handleDrop(e, fileList), false);
+
         inputField.type = 'file';
         inputField.name = 'file';
+        inputField.className = 'dragAndDrop-input-field-neutral';
         inputWrapper.className = 'dragAndDropBox-input-wrapper';
-        inputWrapper.appendChild(inputField);
+        fileList.className = 'dragAnDropBox-file-list';
 
+        inputWrapper.appendChild(inputField);
         dropBox.appendChild(inputWrapper);
+        dropBox.appendChild(fileList);
     });
 };
